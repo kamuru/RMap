a6f9c3823f96f0d2a162e703f3d2fda03cdb6abb
SangHyun Lee <sanghyunnim.lee@samsung.com>
2018-04-20 17:42:52 +0900

    test #1

--- app/src/main/java/com/questlove/kamuru/rmap/MainActivity.java
+++ app/src/main/java/com/questlove/kamuru/rmap/MainActivity.java
@@ -2,6 +2,9 @@ package com.questlove.kamuru.rmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewGroup = (ViewGroup) findViewById(R.id.map_view);
+        mapViewGroup.addView(mapView);
     }
 }
